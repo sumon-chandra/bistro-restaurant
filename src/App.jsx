1e0f3704c937b2e86028e51d29d817e83545a08f
sumon-chandra <topu199041@gmail.com>
2023-05-23 13:21:18 +0600

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
+};
+
+export default App;
