271244957425b0ff37320f724bcb61d2b2714c75
sumon-chandra <topu199041@gmail.com>
2023-05-25 15:02:49 +0600

    Created Our Menu page

--- src/component/sections/Cover.jsx
+++ src/component/sections/Cover.jsx
@@ -2,14 +2,14 @@ const Cover = ({ inHome, img, title, details }) => {
   return (
     <div
       style={{ backgroundImage: `url("${img}")` }}
-      className="lg:mt-20 lg:w-[1320px] lg:h-[500px] lg:px-0 px-4 flex items-center justify-center mx-auto lg:border-0 border shadow-xl lg:py-0"
+      className="lg:mt-20 lg:h-[500px] h-52 lg:px-0 px-4 flex items-center justify-center mx-auto bg-cover"
     >
       <div
         className={`lg:w-3/5 lg:py-10 ${
-          inHome && "bg-white text-black"
-        } bg-[rgba(21, 21, 21, 0.6)] text-white`}
+          inHome ? "bg-white text-black" : "bg-[#151515bb] text-white"
+        }`}
       >
-        <div className="card-body lg:text-center">
+        <div className="card-body text-center">
           <h2 className="uppercase lg:text-4xl text-xl font-cinzel font-normal">
             {title}
           </h2>
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,8 +1,23 @@
 import { Helmet } from "react-helmet-async";
 import PageBanner from "../component/sections/PageBanner";
 import bannerImg from "../assets/menu/banner3.jpg";
+import SectionHead from "./../component/sections/SectionHead";
+import useMenu from "./../hooks/useMenu";
+import MenuCategory from "../component/our-menu/MenuCategory";
+import Cover from "../component/sections/Cover";
+import dessertImg from "../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../assets/menu/pizza-bg.jpg";
+import saladImg from "../assets/menu/salad-bg.jpg";
+import soupImg from "../assets/menu/soup-bg.jpg";
 
 const Menu = () => {
+  const { menu } = useMenu();
+  const offered = menu.filter((item) => item.category === "offered");
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const salads = menu.filter((item) => item.category === "salad");
+  const soups = menu.filter((item) => item.category === "soup");
+
   return (
     <>
       <Helmet>
@@ -15,6 +30,59 @@ const Menu = () => {
           subTitle="Would you like to try a dish?"
         />
       </header>
+      <main className="lg:mt-20 mt-8">
+        <div className="mb-10">
+          <SectionHead heading="Today's offer" subHeading="Don't miss" />
+        </div>
+        {/* ***** Offered Menu Items */}
+        <div className="lg:w-[1320px] px-4 lg:px-0 mx-auto mb-10">
+          <MenuCategory items={offered} />
+        </div>
+        {/* ***** Desserts Menu Items */}
+        <div className="mb-10">
+          <Cover
+            title="Desserts"
+            details="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            img={dessertImg}
+          />
+          <div className="lg:w-[1320px] px-4 lg:px-0 mx-auto mt-10">
+            <MenuCategory items={desserts} />
+          </div>
+        </div>
+        {/* ***** Pizza Menu Items */}
+        <div className="mb-10">
+          <Cover
+            title="Pizza"
+            details="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            img={pizzaImg}
+          />
+          <div className="lg:w-[1320px] px-4 lg:px-0 mx-auto mt-10">
+            <MenuCategory items={pizza} />
+          </div>
+        </div>
+        {/* ***** Salad Menu Items */}
+        <div className="mb-10">
+          <Cover
+            title="Salads"
+            details="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            img={saladImg}
+          />
+          <div className="lg:w-[1320px] px-4 lg:px-0 mx-auto mt-10">
+            <MenuCategory items={salads} />
+          </div>
+        </div>
+        {/* ***** Soup Menu Items */}
+        <div className="mb-10">
+          <Cover
+            title="Soups"
+            details="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            img={soupImg}
+          />
+          <div className="lg:w-[1320px] px-4 lg:px-0 mx-auto mt-10">
+            <MenuCategory items={soups} />
+          </div>
+        </div>
+      </main>
     </>
   );
 };
