1bf891a8837976cdff2ff13dd4d67ed38e1383be
Sumon Chandra <topu199041@gmail.com>
2023-07-30 16:00:56 +0600

    Edit menu category

--- src/component/our-menu/MenuCategory.jsx
+++ src/component/our-menu/MenuCategory.jsx
@@ -6,25 +6,27 @@ const MenuCategory = ({ items }) => {
       <div className="lg:grid grid-cols-2 gap-8 relative pb-24 mt-10">
         {items.map((menuItem) => (
           <div key={menuItem._id}>
-            <div className="flex items-start justify-between mt-4 lg:mt-0">
+            <div className="flex justify-between gap-4 items-center mt-4 lg:mt-0 shadow-md p-4">
               <figure>
                 <img
                   src={menuItem.image}
                   alt=""
-                  className="w-[118px] lg:h-[104px] h-11 object-cover rounded-tr-[200px] rounded-br-[200px] rounded-bl-[200px]  "
+                  className="w-[118px] lg:h-[104px] h-11 object-cover"
                 />
               </figure>
-              <div className="lg:ms-8 ms-2">
+              <div className="">
                 <h3 className="font-cinzel text-lx uppercase font-normal">
                   {menuItem.name} --------
                 </h3>
-                <p>{menuItem.recipe}</p>
+                <p className="text-xs">{menuItem.recipe}</p>
               </div>
-              <h3 className="text-primaryColor text-xl">${menuItem.price}</h3>
+              <h3 className="text-primaryColor text-lg font-bold">
+                ${menuItem.price}
+              </h3>
             </div>
             <div className="mx-auto absolute bottom-4 left-4 text-center w-full">
               <Link
-                to={`/shop/${menuItem.category}`}
+                to={`/shop/salad`}
                 className="uppercase border-b-2 bg-slate-100 py-2 px-4 font-inter font-semibold mt-6 border-gray-700 rounded-md"
               >
                 Order your favorite food
